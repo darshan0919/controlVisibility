dd495adadc5d78cc20d24650373b569243addb2d
Darshan Patel <darshan.patel@sprinklr.com>
2024-10-25 15:47:05 +0530

    import files

--- src/hocs/withVisibility.jsx
+++ src/hocs/withVisibility.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+
+//components
+import { Placeholder } from '../components/Placeholder';
+
+//hooks
 import { useValidateCondition } from '../hooks/useValidateCondition';
 
 // Implement the logic for controlling the visibility of a component based on provided conditions in "conditionsConfig" prop
 /*
-1. When the 'loading' is true, show placeholder
+1. When the 'loading' is true, show placeholder using "Placeholder" component
 2. Once loading is false, hide or show the Component based on 'visible' flag
 */
 
--- src/hooks/useValidateCondition.js
+++ src/hooks/useValidateCondition.js
@@ -1,10 +1,13 @@
 import { useFetchData } from './useFetchData';
 
+//utils
+import { Condition } from '../modules/condition';
+
 export const useValidateCondition = (conditionsConfig) => {
   const { data, loading } = useFetchData(conditionsConfig?.payload);
   const context = { record: data };
 
-  // Implement the logic for checking the condition against a context
+  // Implement the logic for checking the condition against a context using "Condition" class
   return {
     visible: true,
     loading,
