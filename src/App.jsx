0a54519c599318e7cec09da095cce6c724dd53ca
Darshan Patel <darshan.patel@sprinklr.com>
2024-10-25 19:45:57 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ export default function App() {
 
   return (
     <div className="flex flex-col gap-4 widgets-center w-full p-5">
-      <Body enableConfig={enableConfig} />
       <button
         class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2 self-end"
         onClick={() => setEnableConfig((enabled) => !enabled)}
       >
-        {!enableConfig ? 'Show Visible Widgets' : 'Show All Widgets'}
+        {!enableConfig ? 'Show Filtered Widgets' : 'Show All Widgets'}
       </button>
+      <Body enableConfig={enableConfig} />
     </div>
   );
 }
